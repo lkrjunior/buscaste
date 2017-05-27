2df40194572c7972c94959d4ddde77e7256113cc
Luciano Rocha <lucianorocha@MacBook-Air-de-Luciano.local>
2017-05-27 10:55:25 -0300

    Initial Commit

---
+++ AdoteSeuAnimal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdoteSeuAnimal
+//
+//  Created by Luciano Rocha on 27/05/17.
+//  Copyright © 2017 Luciano Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
