859e9d5c663579ace3c6aa211da240a92e6518d0
Luciano Rocha <lucianorocha@MacBook-Air-de-Luciano.local>
2017-08-22 20:57:29 -0300

    Inclusão dos arquivos para controle das interfaces de Filtros e Animais Abandonados

---
+++ AdoteSeuAnimal/FiltrosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FiltrosTableViewCell.swift
+//  AdoteSeuAnimal
+//
+//  Created by Luciano Rocha on 22/08/17.
+//  Copyright © 2017 Luciano Rocha. All rights reserved.
+//
+
+import UIKit
+
+class FiltrosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
