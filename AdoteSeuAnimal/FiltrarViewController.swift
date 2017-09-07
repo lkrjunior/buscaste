bee874d704f85ce8a022a039ae286eacd6df8ae0
Luciano Rocha <lucianorocha@MacBook-Air-de-Luciano.local>
2017-09-06 23:26:13 -0300

    Ajuste inicial na tela de pesquisar

--- AdoteSeuAnimal/FiltrarViewController.swift
+++ AdoteSeuAnimal/FiltrarViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class FiltrarViewController: UIViewController {
+    @IBOutlet weak var txtGenero: UITextField!
+    @IBOutlet weak var txtRaca: UITextField!
+    @IBOutlet weak var txtPorte: UITextField!
+    @IBOutlet weak var txtUf: UITextField!
+    @IBOutlet weak var txtCidade: UITextField!
+    @IBOutlet weak var txtIdadeMin: UITextField!
+    @IBOutlet weak var txtIdadeMax: UITextField!
+    @IBOutlet weak var txtPesoMin: UITextField!
+    @IBOutlet weak var txtPesoMax: UITextField!
+    @IBOutlet weak var switchDoacao: UISwitch!
+    @IBOutlet weak var switchAbandonado: UISwitch!
+    @IBAction func btnLimparClick(_ sender: Any)
+    {
+        
+    }
+    @IBAction func btnVoltarClick(_ sender: Any)
+    {
+        self.showVoltar()
+    }
+    @IBAction func btnPesquisarClick(_ sender: Any)
+    {
+        
+    }
+    
+    func showVoltar()
+    {
+        let tb = self.storyboard?.instantiateViewController(withIdentifier:"TabBarScene") as! TabBarController
+        tb.selectedIndex = 0
+        self.present(tb, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.AjustarCampos()
     }
 
+    func AjustarCampos()
+    {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
